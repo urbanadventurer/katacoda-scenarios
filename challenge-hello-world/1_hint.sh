8dd1c1382cafd70b597bc20371044ba2c21c8e0c
Scott Murray <smurray@oreilly.com>
2021-07-30 10:51:42 -0700

    Test Markdown formatting in hint text

--- challenge-hello-world/1_hint.sh
+++ challenge-hello-world/1_hint.sh
@@ -4,13 +4,13 @@
 
 seconds_sofar=$1
 
-echo "Debug Hint Task 1: $seconds_sofar"
+# echo "Debug Hint Task 1: $seconds_sofar"
 
 if [[ $seconds_sofar -ge 5 &&  $seconds_sofar -lt 10 ]]; then
-  echo "Keep going, a hint will be shown soon..."
+  echo "Still working on it? I'll show a hint very soon..."
 fi
 
 
 if [ $seconds_sofar -ge 10 ]; then
-  echo "Hint: This could give you a clue that could help nudge you toward the solution."
+  echo "**Hint:** Normally, this hint would give help nudge you toward the solution. (As it is, I'm just going to tell you the answer, in case you're new to the command line. The simplest way to create a new file is the command `touch`. Try typing `touch bananas.txt` and then hit enter.)"
 fi
