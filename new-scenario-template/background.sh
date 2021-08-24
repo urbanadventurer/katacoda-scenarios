956fb0e8c9d63ae30bff726e62975ce275c242c0
Scott Murray <smurray@oreilly.com>
2021-08-24 00:23:34 -0700

    Text refinements

--- new-scenario-template/background.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'The file is named background.sh because when it executes, the commands are not echoed to the terminal -- though any output *is* shown to the learner, like this very text.\n\n'
-printf 'To run a script in the foreground, specify it in the index.json as "code": "foreground.sh"\n\n'
