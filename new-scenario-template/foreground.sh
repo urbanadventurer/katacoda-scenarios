280bb1becd960a9a0d798635e1ef5c26f8f6f574
Scott Murray <smurray@oreilly.com>
2021-08-24 00:19:15 -0700

    Text refinements

--- new-scenario-template/foreground.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'The file is named foreground.sh because when it executes, any output is shown to the learner in the terminal.\n\n'
-printf 'To run a script silently, specify it in the index.json as "background": "background.sh"\n\n'
--- new-scenario-template/step3.md
+++ new-scenario-template/step3.md
@@ -4,19 +4,5 @@ This is some text.
 
 Here's a single line of runnable code:
 
-
-`print('hello world')`{{execute}}
-
-Here's a block of runnable code:
-
-```
-a = 1
-b = 2
-c = 3
-d = a + b + c
-
-d
-```{{execute}}
-
-
+`printf "Cello, world!"`{{execute}}
 
