37e01f0daa982cd33689ea1977bdede4357de050
Andrew Horton <horton.nz@gmail.com>
2022-02-06 02:55:13 +1100

    update

--- new-scenario-template/foreground.sh
+++ new-scenario-template/foreground.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-`docker volume create --name=bitcoind-data`
+docker volume create --name=bitcoind-data
 
-`docker run -v bitcoind-data:/bitcoin/.bitcoin --name=bitcoind-node -d \
+docker run -v bitcoind-data:/bitcoin/.bitcoin --name=bitcoind-node -d \
      -p 8333:8333 \
      -p 127.0.0.1:8332:8332 \
-     kylemanna/bitcoind`
+     kylemanna/bitcoind
 
